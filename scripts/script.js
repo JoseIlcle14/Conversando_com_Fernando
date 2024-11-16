f9bf24bc0e2449250879dd18ac44e05fc2507ab7
Jose Ilcle <joseilclemaia@gmail.com>
2024-11-16 06:55:51 -0300

    pagina 2 e js simples

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+// quando a página carregar o site iniciará um texto
+document.addEventListener("DOMContentLoaded", function(){
+    let fala = document.getElementById("falas")
+    fala.innerHTML = ("Este é Fernando, um jovem que está estudando para uma prova de espanhol, ajude-o a estudar, converse com ele e corrijá-o se necessário")
+})
