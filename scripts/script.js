79f601d47e2ce28cbc5ddb6a0db982e1a4664000
Jose Ilcle <joseilclemaia@gmail.com>
2024-11-20 10:42:31 -0300

    criaçao de funções para criar perguntas e botões

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,27 @@
 
 // quando o botão for clicado as falas serão mudadas
-let fala = document.getElementById("falas")
 const botao = document.getElementById('botao')
-
+let resposta = document.getElementById('respostas')
 botao.addEventListener('click', function(){
-    fala.textContent = ("Olá eu sou Fernando")
+    let fala = document.getElementById("falas")
+    fala.textContent = ''
+    resposta.style.display = 'flex'
+    botao.style.display = "none"
 })
+
+// função pra mudar a imagem
+botao.addEventListener('click', function() {
+    let container = document.getElementById("fernando") // minha div
+    let img = container.querySelector("img") // selecionando imagem da div 
+    img.src = "/icons/fernando sorriso aberto.png"
+})
+
+// gera as perguntas pré-estabelecidas
+botao.addEventListener('click', GerarPerguntas )
+function GerarPerguntas(){
+    let perguntas = ['voce gosta de frutas? ', 'tudo bem com vc?', 'gosto de maçãs']
+    let fala = document.getElementById("falas")
+    fala.textContent = (perguntas[0])
+}
+
+
