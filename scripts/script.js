9f1ae007efbbba8589f2ad8e7951caf45369e229
Jose Ilcle <joseilclemaia@gmail.com>
2024-11-21 21:07:11 -0300

    mexendo na fala dnv

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 
 // quando o botão for clicado as falas serão mudadas
 const botao = document.getElementById('botao')
-let resposta = document.getElementById('respostas')
+let resposta = ['Dizer sim', 'ES']
+
 botao.addEventListener('click', function(){
     let fala = document.getElementById("falas")
     fala.textContent = ''
@@ -19,9 +20,18 @@ botao.addEventListener('click', function() {
 // gera as perguntas pré-estabelecidas
 botao.addEventListener('click', GerarPerguntas )
 function GerarPerguntas(){
-    let perguntas = ['voce gosta de frutas? ', 'tudo bem com vc?', 'gosto de maçãs']
+    let perguntas = [
+        '¿En qué contexto utilizamos los verbos imperativos negativos regulares?',
+        '¿Los verbos imperativos negativos regulares terminados en AR tienen terminación en la persona TÚ, son iguales a...?'
+    ]
     let fala = document.getElementById("falas")
     fala.textContent = (perguntas[0])
+    if (perguntas[0]) {
+        let Opção1 = document.getElementById('Opçao1')
+        let Opção2 = document.getElementById('Opçao2')
+        Opção2.value = 'Dizer sim'        
+        Opção1.value = 'Dizer não'
+    }
 }
 
 
